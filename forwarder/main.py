838c766934b84f9535564caadbe18d05a33e3809
Luca Palonca <luca.palonca@sailogy.com>
2021-03-15 22:37:47 +0100

    Fixed

--- forwarder/main.py
+++ forwarder/main.py
@@ -3,14 +3,8 @@ import requests
 
 api_id = 3877185
 api_hash = "a71f3afb87f37c887e2373ee401ffc66"
-TelegramClient('login.session', api_id, api_hash).start()
 
-
-# api_id = 2840733
-# api_hash = "51c7559051fc35ca378e6ea4ed6cccc9"
-
-
-url = "https://firebasestorage.googleapis.com/v0/b/test1-ba648.appspot.com/o/login.session?alt=media&token=978195ad-6c9f-4a9a-92c0-f27d7ac38f0f"
+url = "https://firebasestorage.googleapis.com/v0/b/test1-ba648.appspot.com/o/login.session?alt=media&token=77467350-16b9-4a5d-8321-559397f2e6eb"
 r = requests.get(url, allow_redirects=True)
 open('login.session', 'wb').write(r.content)
 
@@ -23,10 +17,13 @@ FROM_CHATS = [
    -1001148795974,
 ]
 
+DEAL_GROUP = -563703943
+DEV_GROUP = -405845918
+
 with TelegramClient('login.session', api_id, api_hash) as client:
 
    from_entity = [client.get_entity(chat_id) for chat_id in FROM_CHATS]
-   to_chat = client.get_entity(-405845918)
+   to_chat = client.get_entity(DEV_GROUP)
 
    @client.on(events.NewMessage(chats=from_entity))
    async def handler(event):
