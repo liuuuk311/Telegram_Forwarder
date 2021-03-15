906a329b2a2bded7a8372a26e3ba5f3c5569dc41
Luca Palonca <luca.palonca@sailogy.com>
2021-03-15 22:53:51 +0100

    fixed async await

--- forwarder/main.py
+++ forwarder/main.py
@@ -9,24 +9,30 @@ r = requests.get(url, allow_redirects=True)
 open('login.session', 'wb').write(r.content)
 
 FROM_CHATS = [
-   -1001159240979,
-   -1001196885168,
-   -1001161672347,
-   -1001339383475,
-   -1001317129788,
-   -1001148795974,
+    -1001159240979,
+    -1001196885168,
+    -1001161672347,
+    -1001339383475,
+    -1001317129788,
+    -1001148795974,
 ]
 
 DEAL_GROUP = -563703943
 DEV_GROUP = -405845918
 
-with TelegramClient('login.session', api_id, api_hash) as client:
 
-   from_entity = [client.get_entity(chat_id) for chat_id in FROM_CHATS]
-   to_chat = client.get_entity(DEV_GROUP)
+async def get_from_chats(client):
+    return [await client.get_entity(chat_id) for chat_id in FROM_CHATS]
 
-   @client.on(events.NewMessage(chats=from_entity))
-   async def handler(event):
-      await event.message.forward_to(to_chat)
 
-   client.run_until_disconnected()
+async with TelegramClient('login.session', api_id, api_hash) as client:
+    from_entity = await get_from_chats(client)
+    to_chat = client.get_entity(DEV_GROUP)
+
+
+    @client.on(events.NewMessage(chats=from_entity))
+    async def handler(event):
+        await event.message.forward_to(to_chat)
+
+
+    client.run_until_disconnected()
