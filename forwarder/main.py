7184f2ca11d114bfa5c8a70ae46dd2d1455e2eb8
Luca Palonca <luca.palonca@sailogy.com>
2021-03-16 10:11:01 +0100

    New db

--- forwarder/main.py
+++ forwarder/main.py
@@ -1,34 +1,37 @@
 from telethon.sync import TelegramClient, events
 import requests
 
-api_id = 3877185
-api_hash = "a71f3afb87f37c887e2373ee401ffc66"
+api_id = 3052860
+api_hash = "c309b7d34c3c15d0f0dac5b503711e9b"
 
-url = "https://firebasestorage.googleapis.com/v0/b/test1-ba648.appspot.com/o/login.session?alt=media&token=b90380aa-fd6e-4496-81f6-5dd6a6db7d92"
-r = requests.get(url, allow_redirects=True)
-open('login.session', 'wb').write(r.content)
-
-FROM_CHATS = [
-    -1001159240979,
-    -1001196885168,
-    -1001161672347,
-    -1001339383475,
-    -1001317129788,
-    -1001148795974,
-]
-
-DEAL_GROUP = -563703943
-DEV_GROUP = -405845918
+TelegramClient('prod.session', api_id, api_hash).start()
 
 
-client = TelegramClient('login.session', api_id, api_hash)
-client.start()
-client.send_message('me', 'Rubo le offerte con questo account')
-
-
-@client.on(events.NewMessage(chats=FROM_CHATS))
-async def handler(event):
-    to_chat = await client.get_entity(DEAL_GROUP)
-    await event.message.forward_to(to_chat)
-
-client.run_until_disconnected()
+# url = "https://firebasestorage.googleapis.com/v0/b/test1-ba648.appspot.com/o/login.session?alt=media&token=b90380aa-fd6e-4496-81f6-5dd6a6db7d92"
+# r = requests.get(url, allow_redirects=True)
+# open('login.session', 'wb').write(r.content)
+#
+# FROM_CHATS = [
+#     -1001159240979,
+#     -1001196885168,
+#     -1001161672347,
+#     -1001339383475,
+#     -1001317129788,
+#     -1001148795974,
+# ]
+#
+# DEAL_GROUP = -563703943
+# DEV_GROUP = -405845918
+#
+#
+# client = TelegramClient('login.session', api_id, api_hash)
+# client.start()
+# client.send_message('me', 'Rubo le offerte con questo account')
+#
+#
+# @client.on(events.NewMessage(chats=FROM_CHATS))
+# async def handler(event):
+#     to_chat = await client.get_entity(DEAL_GROUP)
+#     await event.message.forward_to(to_chat)
+#
+# client.run_until_disconnected()
