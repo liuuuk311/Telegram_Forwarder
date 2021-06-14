1c79f00ef9f945b784d5d86c180c77bfd32e3438
Luca Palonca <luca.palonca@sailogy.com>
2021-06-14 13:11:04 +0200

    using string session

--- forwarder/main.py
+++ forwarder/main.py
@@ -1,15 +1,21 @@
+from telethon.sessions import StringSession
 from telethon.sync import TelegramClient, events
 import requests
 
 api_id = 3052860
 api_hash = "c309b7d34c3c15d0f0dac5b503711e9b"
 
-# TelegramClient('prod.session', api_id, api_hash).start()
+# Uncomment when we need a new session key
+# with TelegramClient(StringSession(), api_id, api_hash) as client:
+#     session_key = client.session.save()
+#     print(session_key)
+#     exit()
 
+session_key = "1BJWap1sBu14bV-xpw4dDlhMCtl9k_drCaXGaxfNdWVlSoDE233J-i5CU1XvqgUZr4Yn7J1JNPLIPf62FrkamVj3EAJoX7A8jwFfubboK1iHTWI6cvcJwo1yzzZ-UvyM2DZh-_KeJTBwyamRuK36tSxBw2nN4T14BK_EIjMkUGsqKCJwf-7wB49XjyNZMHG9BkEvRlhHiel2k0pYHIzbFDwcDzhRmEadXYfwqI-9WZLCy2aI8eU_kHpMFS5V0UH0pduX55n8S_pj9I_VUSYw4WtRGnu0KosZdUQ1ar-XuQd6n3bnApWwh2gKOHrnbnSJXMwDF5r2zFxKXFFnGOddhq9hLoF8DSBM="
 
-url = "https://firebasestorage.googleapis.com/v0/b/test1-ba648.appspot.com/o/prod.session?alt=media&token=86f917cc-e2e3-4fd8-9dea-1af759b40174"
-r = requests.get(url, allow_redirects=True)
-open('login.session', 'wb').write(r.content)
+# url = "https://firebasestorage.googleapis.com/v0/b/test1-ba648.appspot.com/o/prod.session?alt=media&token=86f917cc-e2e3-4fd8-9dea-1af759b40174"
+# r = requests.get(url, allow_redirects=True)
+# open('login.session', 'wb').write(r.content)
 
 FROM_CHATS = [
     -1001159240979,
@@ -24,7 +30,7 @@ DEAL_GROUP = -563703943
 DEV_GROUP = -405845918
 
 
-client = TelegramClient('login.session', api_id, api_hash)
+client = TelegramClient(StringSession(session_key), api_id, api_hash)
 client.start()
 client.send_message('me', 'Rubo le offerte con questo account')
 
