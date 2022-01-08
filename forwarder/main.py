47dccb56a1c94f86fdc4667af97bf58a3e3d7496
Luca Palonca <luca.palonca@sailogy.com>
2022-01-08 18:58:12 +0100

    Test

--- forwarder/main.py
+++ forwarder/main.py
@@ -67,7 +67,7 @@ allowed_to_send_amazon_tracking_command = [
 #     await event.message.forward_to(to_chat)
 
 
-@client.on(events.NewMessage(incoming=True))
+@client.on(events.NewMessage(incoming=True, from_users=["@MisterPrezzo"]))
 async def generic_handler(event: events.NewMessage.Event):
     logger.info(f"Message received from chat_id: {event.chat_id} message: {event.message.message}")
 
