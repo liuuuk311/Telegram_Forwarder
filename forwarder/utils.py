4010a249e0628918e890c9ba35671486ab0a6c10
Luca Palonca <luca.palonca@sailogy.com>
2022-01-10 07:41:52 +0100

    Test

--- forwarder/utils.py
+++ forwarder/utils.py
@@ -13,4 +13,6 @@ def extract_links(entities: List) -> List[str]:
     logger.info(entities)
     filtered = filter(lambda x: isinstance(x, MessageEntityTextUrl), entities)
     logger.info(list(filtered))
+    for e in filtered:
+        logger.info(e.url)
     return list(map(lambda x: x.url, filtered))
