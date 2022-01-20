931367ef46e273765b5802c32b7d84bfb7afe828
Luca Palonca <luca.palonca@sailogy.com>
2022-01-20 10:18:40 +0100

    Fix

--- forwarder/images.py
+++ forwarder/images.py
@@ -58,7 +58,7 @@ def create_our_image(image_filename: str, price: str, old_price: str, template_n
     if img.shape[0] > img.shape[1]:
         img = image_resize(img, height=700)
     else:
-        img = image_resize(img, width=1000)
+        img = image_resize(img, width=700)
 
     template = cv2.imread(template_name, cv2.IMREAD_UNCHANGED)
     x_offset = max(500 - int(math.floor(img.shape[1] / 2)), 0)
@@ -82,7 +82,7 @@ def create_our_image(image_filename: str, price: str, old_price: str, template_n
 if __name__ == '__main__':
     from utils import get_amazon_image_from_page
     urls = [
-        "https://www.amazon.it/dp/B07Z9M6VL6/?tag=jikemaster-21&psc=1",
+        "https://www.amazon.it/dp/B09645LC84/?tag=jikemaster-21&psc=1",
     ]
     for url in urls:
         img_url = download_image(get_amazon_image_from_page(url))
