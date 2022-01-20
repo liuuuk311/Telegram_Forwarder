b5a6bc4e8d78604b69eb955506ec80657ae53872
Luca Palonca <luca.palonca@sailogy.com>
2022-01-20 09:16:17 +0100

    Fix

--- forwarder/images.py
+++ forwarder/images.py
@@ -24,20 +24,43 @@ def download_image(url: str) -> Optional[str]:
     return DEFAULT_DOWNLOADED_IMAGE
 
 
+def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # return the resized image
+    return cv2.resize(image, dim, interpolation=inter)
+
+
 def create_our_image(image_filename: str, price: str, old_price: str, template_name: str = "template.png"):
     img = cv2.imread(image_filename)
 
-    scale_percent = 50
-    width = int(img.shape[1] * scale_percent / 100)
-    height = int(img.shape[0] * scale_percent / 100)
-    dim = (width, height)
-
-    # resize image
-    img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    img = image_resize(img, height=700)
 
     template = cv2.imread(template_name)
     x_offset = 500 - int(math.floor(img.shape[1] / 2))
-    y_offset = int(math.floor(template.shape[0] / 2)) - int(math.floor(img.shape[0] / 2))
+    y_offset = int(math.floor(template.shape[0] / 2)) - int(math.floor(img.shape[0] / 2)) + 100
     template[y_offset:y_offset + img.shape[0], x_offset:x_offset + img.shape[1]] = img
     output_filename = ''.join(random.choices(string.ascii_letters, k=15)) + '.png'
     cv2.imwrite(output_filename, template)
@@ -46,9 +69,9 @@ def create_our_image(image_filename: str, price: str, old_price: str, template_n
     font = ImageFont.truetype('font/Montserrat/static/Montserrat-SemiBold.ttf', 100)
     small_font = ImageFont.truetype('font/Montserrat/static/Montserrat-SemiBold.ttf', 40)
     image_editable = ImageDraw.Draw(my_image)
-    image_editable.text((1080, 270), f"{price}", (255, 255, 255), font=font)
+    image_editable.text((1090, 270), f"{price}", (255, 255, 255), font=font)
     if old_price:
-        image_editable.text((1080, 370), f"al posto di {old_price}", (255, 255, 255), font=small_font)
+        image_editable.text((1090, 370), f"al posto di {old_price}", (255, 255, 255), font=small_font)
     my_image.save(output_filename)
 
     return output_filename
@@ -62,5 +85,4 @@ if __name__ == '__main__':
     ]
     for url in urls:
         img_url = download_image(get_amazon_image_from_page(url))
-        print(img_url)
         create_our_image(img_url, template_name="generic_template.jpeg", price="9.99", old_price="1000")
